af6cff14fcd194ce16cc209e071e5da1cabc43b8
Bennett Feely <bfeelyweb@gmail.com>
2020-11-29 23:07:17 -0500

    Add react + vue implementations

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 ![ztext.js](https://bennettfeely.com/ztext/img/logo.png)
 
-## Easy to implement, 3D typography for any website and any font.
+## 3D typography for any website and any font
 
 Live: https://bennettfeely.com/ztext
 
 Ztext gives the illusion of volume by creating layers from an HTML element. There's no need to spend hours fiddling with `<canvas>` or forcing users to download multi-megabyte WebGL libraries. Content remains fully selectable and accessible.
+
+### Frameworks
+
+For using React implementation, see [react-ztext](https://github.com/snettah/react-ztext) by [@snettah](https://github.com/snettah).
+
+For using Vue implementation, see [vue-ztext](https://github.com/snettah/vue-ztext) by [@Namchee](https://github.com/Namchee).
---
+++ src/js/github.js
@@ -0,0 +1,59 @@
+const right_bow = new Date();
+const times = [
+	["second", 1],
+	["minute", 60],
+	["hour", 3600],
+	["day", 86400],
+	["week", 604800],
+	["month", 2592000],
+	["year", 31536000],
+];
+
+function formatTime(date) {
+	var date = new Date(date);
+
+	var diff = Math.round((right_bow - date) / 1000);
+	for (var t = 0; t < times.length; t++) {
+		if (diff < times[t][1]) {
+			if (t == 0) {
+				return "Just now";
+			} else {
+				diff = Math.round(diff / times[t - 1][1]);
+				return diff + " " + times[t - 1][0] + (diff == 1 ? " ago" : "s ago");
+			}
+		}
+	}
+}
+
+function makeCard(repo) {
+	const xhr = new XMLHttpRequest();
+	const url = "https://api.github.com/repos/" + repo;
+
+	xhr.open("GET", url, true);
+
+	xhr.onload = function () {
+		const respo = JSON.parse(this.response);
+
+		let card = `<div class="repo-box">
+	<div class="repo-title">
+		<a class="repo-author" href="${respo.owner.html_url}">${
+			respo.owner.login
+		}</a> / <a class="repo-name" href="${respo.html_url}">${respo.name}</a>
+	</div>
+	<a href="${respo.html_url}" class="repo-meta">
+		<span class="repo-stars">${respo.stargazers_count} stars</span>
+		<span class="repo-license">MIT</span>
+		<span class="repo-updated">${formatTime(respo.pushed_at)}</span>
+	</a>
+</div>`;
+
+		document.querySelector(`[data-repo="${repo}"]`).innerHTML = card;
+	};
+
+	xhr.send();
+}
+
+// Find each repo wrapper and fill it with an updated repo card
+document.querySelectorAll("[data-repo]").forEach((elem) => {
+	makeCard(elem.dataset.repo);
+});
